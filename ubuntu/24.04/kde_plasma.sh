798347e29ae37a3e4e5c6e58b19e9a74997d105d
AJ-Holzer <axel123.holzer@gmail.com>
2025-10-18 17:36:11 +0200

    **Add Brave browser to KDE Plasma setup scripts**
    - Updated the README.md table to include Brave as a supported program.
    - Modified the `kde_plasma.sh` script to include instructions and installation for the Brave browser.

--- ubuntu/24.04/kde_plasma.sh
+++ ubuntu/24.04/kde_plasma.sh
@@ -47,3 +47,10 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packa
     sudo tee /etc/apt/sources.list.d/vscode.list
 sudo apt update
 sudo apt install -y code
+
+# Brave
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | \
+    sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt install -y brave-browser

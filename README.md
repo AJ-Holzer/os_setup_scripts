70a76087a1c786d4c2004ca54dc17d53aee99906
AJ-Holzer <axel123.holzer@gmail.com>
2025-10-18 19:39:53 +0200

    Update SHA-256 hash for `kde_plasma.sh` and enhance script permissions.

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ curl -fsSL "https://raw.githubusercontent.com/AJ-Holzer/os_setup_scripts/refs/he
 sha256sum kde_plasma.sh
 ```
 
-Verify that the output hash matches this SHA-256 hash: `5118a1bc20742a24ab2ecfaaf58925f85ae816e88073370e88f473bd6cb63e81`
+Verify that the output hash matches this SHA-256 hash: `bbdb32f8c9265a7ae220540dd82578a88c406e1c871e034a7e29e761f9845a59`
 
 #### Start Setup
 
--- ubuntu/24.04/kde_plasma.sh
+++ ubuntu/24.04/kde_plasma.sh
@@ -1,74 +1,81 @@
 #!/usr/bin/env bash
+
+# Request permissions
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
 set -euo pipefail
 
 # Update
-sudo apt update
-# sudo apt upgrade
-pkcon update
+apt update
+# apt upgrade
+# pkcon update
 
 # Install snap and Flatpak
 echo "🟢 Installing Snap..."
-sudo apt install -y snap
+apt install -y snap
 echo "🟢 Installing Flatpak..."
-sudo apt install -y flatpak
+apt install -y flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 
 # === Install Drivers ===
 echo "🟢 Installing drivers..."
-sudo ubuntu-drivers install
+ubuntu-drivers install
 
 
 # === Change Desktop Mode ===
 # Change from wayland to x11
 if ! grep -q "Session=plasma.desktop" /etc/sddm.conf 2>/dev/null; then
     echo "🟢 Switching KDE to X11 session..."
-    sudo sddm --example-config > /etc/sddm.conf
-    sudo sed -i 's/^Session=.*/Session=plasma.desktop/' /etc/sddm.conf
+    sddm --example-config > /etc/sddm.conf
+    sed -i 's/^Session=.*/Session=plasma.desktop/' /etc/sddm.conf
 fi
 
 
 # === Install programs ===
 # Install Spotify
 echo "🟢 Installing Spotify..."
-curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get install spotify-client
+curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb https://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
+apt-get install spotify-client
 spotify
 read -n 1 -s -r -p "🟠 Please login to spotify and press any key to continue..."
 
 # Install Signal
 echo "🟢 Installing Signal..."
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg;
-cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+cat signal-desktop-keyring.gpg | tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
 wget -O signal-desktop.sources https://updates.signal.org/static/desktop/apt/signal-desktop.sources;
-cat signal-desktop.sources | sudo tee /etc/apt/sources.list.d/signal-desktop.sources > /dev/null
-sudo apt update && sudo apt install signal-desktop
+cat signal-desktop.sources | tee /etc/apt/sources.list.d/signal-desktop.sources > /dev/null
+apt update && apt install signal-desktop
 
 # Discord
 flatpak install -y flathub com.discordapp.Discord
 
 # VSCode
 echo "🟢 Installing VSCode..."
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft.gpg > /dev/null
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /usr/share/keyrings/microsoft.gpg > /dev/null
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | \
-    sudo tee /etc/apt/sources.list.d/vscode.list
-sudo apt update
-sudo apt install -y code
+    tee /etc/apt/sources.list.d/vscode.list
+apt update
+apt install -y code
 
 # Brave
 echo "🟢 Installing Brave..."
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | \
-    sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt install -y brave-browser
+    tee /etc/apt/sources.list.d/brave-browser-release.list
+apt install -y brave-browser
 
 
 # === Shortcuts ===
 # Back up shortcut file
 echo "🟢 Backing up global shortcuts..."
 mkdir -p ~/.config/backup_shortcuts
-sudo cp ~/.config/kglobalshortcutsrc ~/.config/backup_shortcuts/kglobalshortcutsrc.bak
+cp ~/.config/kglobalshortcutsrc ~/.config/backup_shortcuts/kglobalshortcutsrc.bak
 
 # Import shortcuts
 echo "🟢 Overwriting global shortcuts..."
